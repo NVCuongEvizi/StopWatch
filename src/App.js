59c3c91aca96f1f85ac30076e5967aa19fbc26df
NVCuongEvizi <cuong.v.nguyen@evizi.com>
2022-10-05 16:31:10 +0700

    push code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [time, setTime] = useState(0)
+  const [isClockRun, setIsClockRun] = useState(false)
+  const timeRef = useRef('')
+
+  const handleStart = () => {
+    setIsClockRun(!isClockRun)
+    timeRef.current = setInterval(() => {
+      setTime(prevTime => prevTime + 1)
+    }, 1000)
+  }
+
+  const handleStop = () => {
+    setIsClockRun(!isClockRun)
+    clearInterval(timeRef.current)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Time: {time}</h2>
+
+      <button
+        disabled={isClockRun}
+        style={{ marginRight: "10px", pointerEvents: isClockRun ? 'none' : 'auto' }}
+        onClick={handleStart}
+      >Start</button>
+
+      <button
+        disabled={!isClockRun}
+        style={{ pointerEvents: isClockRun ? 'auto' : 'none' }}
+        onClick={handleStop}
+      >Stop</button>
     </div>
   );
 }
