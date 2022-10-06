5a1e8d2a7071d6844fa58a11af6447186eeb5a8a
NVCuongEvizi <cuong.v.nguyen@evizi.com>
2022-10-06 11:59:15 +0700

    add cleanup func and UI

--- src/App.js
+++ src/App.js
@@ -1,21 +1,65 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
   const [time, setTime] = useState(0)
   const [isClockRun, setIsClockRun] = useState(false)
+  const [isResumePause, setIsResumePause] = useState(false)
+  const [isStartDisable, setIsStartDisable] = useState(false)
+  const [recordList, setRecordList] = useState([])
   const timeRef = useRef('')
 
+  useEffect(() => {
+    return () => {
+      clearInterval(timeRef.current)
+    }
+  }, [])
+
   const handleStart = () => {
-    setIsClockRun(!isClockRun)
+    setIsStartDisable(true)
+    setIsClockRun(true)
+    setIsResumePause(true)
     timeRef.current = setInterval(() => {
+      console.log('interval running...')
       setTime(prevTime => prevTime + 1)
     }, 1000)
   }
 
-  const handleStop = () => {
-    setIsClockRun(!isClockRun)
+  const handleReset = () => {
+    setTime(0)
+    setIsResumePause(false)
+    setIsStartDisable(false)
+    setIsClockRun(false)
     clearInterval(timeRef.current)
+  }
+
+  const handlePause = () => {
+    setIsClockRun(false)
+    setIsResumePause(true)
+    clearInterval(timeRef.current)
+  }
+
+  const handleStop = () => {
+    setRecordList(prevState => [...prevState, { watchTime: formatDate(), time: time + 's' }])
+    setIsStartDisable(true)
+    setIsClockRun(false)
+    setIsResumePause(false)
+    clearInterval(timeRef.current)
+  }
+
+  const formatDate = () => {
+    const today = new Date()
+    const year = today.getFullYear()
+    let month = today.getMonth()
+    let day = today.getDate()
+    if (month < 10) {
+      month = '0' + month
+    }
+    if (day < 10) {
+      day = '0' + day
+    }
+    return day + '/' + month + '/' + year
   }
 
   return (
@@ -23,16 +67,33 @@ function App() {
       <h2>Time: {time}</h2>
 
       <button
-        disabled={isClockRun}
-        style={{ marginRight: "10px", pointerEvents: isClockRun ? 'none' : 'auto' }}
+        disabled={isStartDisable}
+        style={{ pointerEvents: isStartDisable ? 'none' : 'auto' }}
         onClick={handleStart}
       >Start</button>
+
+      <button
+        onClick={handleReset}
+      >Reset</button>
+
+      <button
+        disabled={!isResumePause}
+        style={{ pointerEvents: isResumePause ? 'auto' : 'none' }}
+        onClick={isClockRun ? handlePause : handleStart}
+      >{isClockRun ? 'Pause' : 'Resume'}</button>
 
       <button
         disabled={!isClockRun}
         style={{ pointerEvents: isClockRun ? 'auto' : 'none' }}
         onClick={handleStop}
       >Stop</button>
+
+      <ul>
+        <h2>Your time record:</h2>
+        {recordList?.map((item, index) =>
+          <li key={index}>#{index + 1} - watchTime: {item.watchTime} - Time: {item.time}</li>
+        )}
+      </ul>
     </div>
   );
 }
